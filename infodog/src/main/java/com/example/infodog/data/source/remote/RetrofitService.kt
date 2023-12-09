100ac7e0743ef40cf30f70e5879d1b144ca1b767
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2023-12-09 17:05:16 -0300

    Criação de interface de serviço com endpoint para buscar as raças de cães #4

--- infodog/src/main/java/com/example/infodog/data/source/remote/RetrofitService.kt
+++ infodog/src/main/java/com/example/infodog/data/source/remote/RetrofitService.kt
@@ -2,6 +2,7 @@ package com.example.infodog.data.source.remote
 
 import com.example.infodog.BuildConfig
 import com.example.infodog.data.model.Breed
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -15,5 +16,5 @@ interface RetrofitService {
         @Query("page") page: Int = DEFAULT_PAGE,
         @Query("limit") limit: Int = DEFAULT_LIMIT,
         @Query("api_key") apikey: String = BuildConfig.API_KEY
-    ): List<Breed>
+    ): Response<List<Breed>>
 }
