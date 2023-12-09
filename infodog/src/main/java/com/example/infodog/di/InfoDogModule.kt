12a656c4f2fdc01c8f1f6aa7bf78dc72fe674ee7
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2023-12-09 18:44:16 -0300

    Criação do GetBreedsUseCase #8

--- infodog/src/main/java/com/example/infodog/di/InfoDogModule.kt
+++ infodog/src/main/java/com/example/infodog/di/InfoDogModule.kt
@@ -1,15 +1,19 @@
 package com.example.infodog.di
 
+import androidx.paging.PagingData
+import com.example.infodog.data.model.Breed
 import com.example.infodog.data.repository.breed.BreedRemoteRepository
+import com.example.infodog.data.repository.breed.BreedRepository
 import com.example.infodog.data.source.remote.RetrofitClientFactory
 import com.example.infodog.data.source.remote.paging.BreedPagingSource
 import com.example.infodog.router.InfoDogRouter
 import com.example.infodog.router.InfoDogRouterImpl
+import kotlinx.coroutines.flow.Flow
 import org.koin.dsl.module
 
 val infoDogModule = module {
     single<InfoDogRouter> { InfoDogRouterImpl() }
     single { RetrofitClientFactory() }
     single { BreedPagingSource(get()) }
-    single { BreedRemoteRepository(get()) }
+    single<BreedRepository<Flow<PagingData<Breed>>>> { BreedRemoteRepository(get()) }
 }
